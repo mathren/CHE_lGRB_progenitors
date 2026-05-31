b1fd7350b55a5c954f3bd6b32054fb43b4128d8d
mrenzo <mrenzo@arizona.edu>
2026-05-31 12:48:23 -0700

    test download script

--- scripts/data_prep.sh
+++ scripts/data_prep.sh
@@ -4,14 +4,17 @@ set -e
 cd ./data
 echo "   Downloading data from https://doi.org/10.5281/zenodo.14286306"
 echo "    ⚠️ Warning: this will use ~8GB"
-# wget -U firefox https://zenodo.org/records/11375523/files/grid.tar.xz
+wget -U firefox -c --header="Authorization: Bearer $ZENODO_TOKEN" \
+  "https://zenodo.org/api/records/14286306/draft/files/grid.tar/content" || \
+    echo "The repo may still be private. Remember to export ZENODO_TOKEN=<your token> and verify you have access."
 echo "   Unpacking data"
-# tar xzvf grid.tar
+mv content grid.tar
+tar xzvf grid.tar
 echo "    ⚠️ Warning: this will use ~15GB"
 for f in *.*rot*.tar.xz; do
     echo "     Create subfolder and unpack $f"
     mkdir -p "${f%.tar.xz}"
-    # tar -xzf "$f" -C "${f%.tar.xz}"
+    tar -xzf "$f" -C "${f%.tar.xz}"
 done
 # tar xzvf grid.tar
 echo "   Downloading data from https://doi.org/10.5281/zenodo.11375523"
