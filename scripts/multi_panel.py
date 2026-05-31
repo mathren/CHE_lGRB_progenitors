418d0e4567bb84718453966f937527e2f12d65c5
mrenzo <mrenzo@arizona.edu>
2026-05-31 11:31:18 -0700

    multi_panel fixes
    add small net in all panels

--- scripts/multi_panel.py
+++ scripts/multi_panel.py
@@ -31,7 +31,8 @@ if __name__ == "__main__":
     ax1 = fig.add_subplot(gs[1])
     ax2 = fig.add_subplot(gs[2])
     ax3 = fig.add_subplot(gs[3])
-    zx = ax2.inset_axes([0.13, 0.11, 0.68, 0.4])  # for x=logM
+    zx = ax2.inset_axes([0.13, 0.11, 0.68, 0.4])
+
     for mod in models:
         hfile = mod+"/LOGS/history.data"
         M, o = get_model_initial_values(mod)
@@ -45,6 +46,8 @@ if __name__ == "__main__":
             ax0.plot(logT, logL, lw=3, c='C1',zorder=10)
 
         # density -----------------------------
+        pfile = mod+"LOGS/CHE_single_core_collapse.data"
+        src, col = get_src_col(pfile)
         logrho = src[:, col.index("logRho")]
         m = src[:, col.index("mass")]
         ax2.plot(m, logrho, c='C0', alpha=0.3, lw=0.5)
@@ -64,8 +67,6 @@ if __name__ == "__main__":
             ax3.plot(m, j_specific, lw=3, c='C1', zorder=10)
 
         # Ye ----------------------------------------------------
-        pfile = mod+"LOGS/CHE_single_core_collapse.data"
-        src, col = get_src_col(pfile)
         m = src[:, col.index("mass")]
         ye = src[:, col.index('ye')]
         ax1.plot(m, ye, c='C0', alpha=0.3, lw=0.5, zorder=1)
@@ -76,24 +77,33 @@ if __name__ == "__main__":
                 small_net = "../data/SMALL_NET/40_rot0.6_small_net/LOGS1/CHE_single_core_collapse.data"
                 src, col = get_src_col(small_net)
                 m = src[:, col.index("mass")]
+                logrho = src[:, col.index("logRho")]
                 ye = src[:, col.index('ye')]
-                ax1.plot(m, ye, c='k', ls='-.', lw=1,
-                         zorder=10,
+                logr = src[:, col.index("logR")]
+                r = 10.0**logr
+                omega = src[:, col.index("omega")]
+                j_specific = r*r*omega
+                ax1.plot(m, ye, c='k', ls='-.', lw=0.5, zorder=10,
                          label=r"$40\,M_{\odot},\ \frac{\omega_{\rm ZAMS}}{\omega_{\rm crit}}=0.6$"+"\n small network")
+                ax2.plot(m, logrho, lw=0.5, c='k', ls='-.', zorder=10)
+                zx.plot(m, logrho, lw=0.5, ls='-.', c='k', zorder=10)
+                ax3.plot(m, j_specific, lw=0.5, c='k', ls='-.', zorder=10)
                 ax1.legend(fontsize=20, handletextpad=0.1, frameon=True)
-            except:
+                hfile =  "../data/SMALL_NET/40_rot0.6_small_net/LOGS1/history.data"
+                src, col = get_src_col(hfile)
+                logT = src[:, col.index("log_Teff")]
+                logL = src[:, col.index("log_L")]
+                ax0.plot(logT, logL, lw=0.5, ls='-.', c='k',zorder=10)
+            except FileNotFoundError:
                 print("No small net model")
                 print("This model is available at https://zenodo.org/records/11375523")
                 print("Download and unpack in ./data/SMALL_NET/")
                 pass
 
-
-
     ax0.invert_xaxis()
     ax0.set_xlabel(r"$\log_{10}(T_\mathrm{eff}/[K])$")
     ax0.set_ylabel(r"$\log_{10}(L/L_\odot)$")
 
-    # ax1.set_xscale('log')
     ax1.set_xlim(0, 4)
     ax1.set_ylim(0.44, 0.505)
     ax1.set_ylabel(r"$Y_e=\sum_j\,X_j Z_j / A_j$")
