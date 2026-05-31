ef4a29f815e9ffab72d988e4a9a930118fbdf7c3
mrenzo <mrenzo@arizona.edu>
2026-05-31 09:56:09 -0700

    download script and abstract

--- scripts/data_prep.sh
+++ scripts/data_prep.sh
@@ -8,13 +8,14 @@ echo "    ⚠️ Warning: this will use ~8GB"
 echo "   Unpacking data"
 # tar xzvf grid.tar
 echo "    ⚠️ Warning: this will use ~15GB"
-for f in *.tar.xz; do
+for f in *.*rot*.tar.xz; do
     echo "     Create subfolder and unpack $f"
     mkdir -p "${f%.tar.xz}"
     # tar -xzf "$f" -C "${f%.tar.xz}"
 done
 # tar xzvf grid.tar
 echo "   Downloading data from https://doi.org/10.5281/zenodo.11375523"
+echo "   for 22-isotope network model"
 mkdir -p ./SMALL_NET/
 cd ./SMALL_NET
 wget -U firefox https://zenodo.org/records/11375523/files/40_rot0.6_small_net.tar.xz
